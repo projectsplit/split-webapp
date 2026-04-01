ef611440b0fddf6c9bc0fbb028596a950081042d
ChristosKehayas <christos.kechagia@gmail.com>
2026-04-02 00:41:52 +0100

    pwa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.g3t4c0su5to"
+    "revision": "0.acbradhvjkg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
